38f221a7a96b679b43d962a382f26a77903ed73f
adelbert00 <tazor1337@gmail.com>
2024-06-02 11:43:35 +0200

    CSS CLASS CHANGES
    CSS PREPARED FOR TAILWIND TRANSLATE

--- Web01/src/App.vue
+++ Web01/src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="app">
+
     <!-- Navigation -->
-    <nav class="navbar">
-      <button class="navbutton" @click="scrollToSection('about')">About</button>
-      <button class="navbutton" @click="scrollToSection('reels')">Reel</button>
-      <button class="navbutton" @click="scrollToSection('music')">Music</button>
-      <button class="navbutton" @click="scrollToSection('sound-effects')">SFX</button>
-      <button class="navbutton" @click="scrollToSection('contact')">Contact</button>
+    <nav class="Navbar">
+      <button class="Navbutton" @click="scrollToSection('about')">About</button>
+      <button class="Navbutton" @click="scrollToSection('reels')">Reel</button>
+      <button class="Navbutton" @click="scrollToSection('music')">Music</button>
+      <button class="Navbutton" @click="scrollToSection('sound-effects')">SFX</button>
+      <button class="Navbutton" @click="scrollToSection('contact')">Contact</button>
     </nav>
+    <!-- End of Nav -->
 
     <!-- Header Section -->
     <section>
@@ -19,115 +21,124 @@
         <img :src="imageSrc" alt="My Photo">
       </header>
     </section>
+    <!-- End of Header -->
 
     <!-- About Section -->
     <section>
       <div id="About-Me">
-        <h1 class="H1Name">ABOUT ME</h1>
+        <h1 class="H1-Head">ABOUT ME</h1>
         <p class="PDescribe">
           With nearly a decade of experience in sound manipulation, specializing in music production, game sound design, film scoring, and crafting audio for commercials and video clips. Committed to integrating modern solutions. Dedicated to fostering an enjoyable work environment while pushing boundaries and embracing new challenges. Constantly seeking to innovate and bring fresh perspectives to every project, whether through cutting-edge technologies or refined traditional methods.
         </p>
       </div>
     </section>
+    <!-- End of About -->
 
     <!-- Sample Packs Section -->
-    <section id="samplepacks">
-      <h1 class="headersample">SAMPLE PACKS</h1>
-      <div class="reelsframes">
-        <div class="reeldiv" v-for="(pack, index) in samplePacks" :key="index">
+    <section>
+      <h1 class="H1-Head">SAMPLE PACKS</h1>
+      <div id="Reelsframes">
+        <div class="Reeldiv" v-for="(pack, index) in samplePacks" :key="index">
           <img :src="pack.imageSrc" class="samplepackimg" :alt="pack.name">
-          <p class="opismuza">{{ pack.name }}</p>
-          <p class="opismuza2">Contains</p>
-          <p v-for="(content, cIndex) in pack.contents" :key="cIndex" class="opismuza3">{{ content }}</p>
+          <p class="Describe1">{{ pack.name }}</p>
+          <p class="Describe2">Contains</p>
+          <p v-for="(content, cIndex) in pack.contents" :key="cIndex" class="Describe3">{{ content }}</p>
           <br>
-          <a :href="pack.previewLink" class="buttonsamplepack">PREVIEW</a>
+          <a :href="pack.previewLink" class="Button-SP">PREVIEW</a>
         </div>
       </div>
     </section>
+    <!-- End of Sample Packs -->
 
     <!-- Reels Section -->
-    <section id="reels">
-      <h1 class="headerh1">SOUND DESIGN DEMOS WITH VIDEO</h1>
+    <section>
+      <h1 class="H1-Head">SOUND DESIGN DEMOS WITH VIDEO</h1>
       <div class="reelsframes">
         <div class="reeldiv" v-for="reel in reels" :key="reel.title">
           <iframe :width="reel.width" :height="reel.height" :src="reel.src" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-          <p class="opismuza">{{ reel.title }}</p>
-          <p class="opismuza2">{{ reel.subtitle }}</p>
+          <p class="Describe1">{{ reel.title }}</p>
+          <p class="Describe2">{{ reel.subtitle }}</p>
         </div>
       </div>
     </section>
+    <!-- End of Reels -->
 
-    <!-- Music Section -->
-    <section id="music">
-      <h1 class="headerh1">MUSIC</h1>
-      <div class="musicframes">
-        <div class="musicdiv" v-for="music in musicList" :key="music.title">
+    <!-- Music Reels Section -->
+    <section>
+      <h1 class="H1-Head">MUSIC</h1>
+      <div class="Music-Frames">
+        <div class="Music-Div" v-for="music in musicList" :key="music.title">
           <iframe :width="music.width" :height="music.height" :src="music.src" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-          <p class="opismuza">Produced by:</p>
-          <p class="opismuza2">{{ music.producer }}</p>
-          <p class="opismuza3">Copyright ©</p>
-          <p class="opismuza4">{{ music.rights }}</p>
+          <p class="Describe1">Produced by:</p>
+          <p class="Describe2">{{ music.producer }}</p>
+          <p class="Describe3">Copyright ©</p>
+          <p class="Describe4">{{ music.rights }}</p>
         </div>
       </div>
     </section>
+    <!-- End of Music Reels -->
 
     <!-- Music Scores Section -->
-    <section id="scoremusic">
-      <h1 class="headerh2">MUSIC SCORES</h1>
-      <div class="examplemusic">
-        <div class="examplediv" v-for="track in musicTracks" :key="track.src">
+    <section>
+      <h1 class="H1-Head">MUSIC SCORES</h1>
+      <div class="Music-Scores">
+        <div class="Scores-Div" v-for="track in musicTracks" :key="track.src">
           <audio controls>
             <source :src="track.src" type="audio/mpeg" />
           </audio>
-          <p class="opismuza">{{ track.title }}</p>
-          <p class="opismuza4">{{ track.details }}</p>
+          <p class="Describe1">{{ track.title }}</p>
+          <p class="Describe4">{{ track.details }}</p>
         </div>
       </div>
     </section>
+    <!-- End of Music Scores -->
 
     <!-- Sound Effects Section -->
-    <section id="sound-effects">
-      <h1 class="headerh1">SOUND EFFECTS</h1>
-      <div class="sfxdiv">
-        <div class="audiocontrols3" v-for="sfx in soundEffects" :key="sfx.src">
+    <section>
+      <h1 class="H1-Head">SOUND EFFECTS</h1>
+      <div class="Sfx-Div">
+        <div class="Audio-Controls" v-for="sfx in soundEffects" :key="sfx.src">
           <audio controls>
             <source :src="sfx.src" :type="sfx.type" />
           </audio>
-          <p class="opismuza">{{ sfx.title }}</p>
-          <p class="opismuza2">{{ sfx.details }}</p>
+          <p class="Describe1">{{ sfx.title }}</p>
+          <p class="Describe2">{{ sfx.details }}</p>
         </div>
       </div>
     </section>
+    <!-- End of SFX-->
 
     <!-- Foley Examples Section -->
-    <section id="foley-examples">
-      <h1 class="headerh1">FOLEY EXAMPLES</h1>
-      <div class="sfxdiv">
-        <div class="audiocontrols3" v-for="foley in foleyExamples" :key="foley.src">
+    <section>
+      <h1 class="H1-Head">FOLEY EXAMPLES</h1>
+      <div class="Foley-Div">
+        <div class="Audio-Controls" v-for="foley in foleyExamples" :key="foley.src">
           <audio controls>
             <source :src="foley.src" :type="foley.type" />
           </audio>
-          <p class="opismuza">{{ foley.title }}</p>
+          <p class="Describe1">{{ foley.title }}</p>
         </div>
       </div>
     </section>
+    <!-- End of Foley -->
 
     <!-- Percs Examples Section -->
-    <section id="percs-examples">
-      <h1 class="headerh1">PERCS EXAMPLES</h1>
-      <div class="sfxdiv">
-        <div class="audiocontrols3" v-for="perc in percExamples" :key="perc.src">
+    <section>
+      <h1 class="H1-Head">PERCS EXAMPLES</h1>
+      <div class="Percs-Div">
+        <div class="Audio-Controls" v-for="perc in percExamples" :key="perc.src">
           <audio controls>
             <source :src="perc.src" :type="perc.type" />
           </audio>
-          <p class="opismuza">{{ perc.title }}</p>
+          <p class="Describe1">{{ perc.title }}</p>
         </div>
       </div>
     </section>
+    <!-- End of Percs -->
 
     <!-- Contact Section -->
-    <section id="contact">
-      <h1 class="headerh1">CONTACT</h1>
+    <section id="Contact">
+      <h1 class="H1-Head">CONTACT</h1>
       <form @submit.prevent="handleSubmit">
         <label for="name" class="labels">Imię:</label>
         <input type="text" id="name" v-model="formData.name" required />
@@ -138,13 +149,14 @@
         <label for="message" class="labels">Wiadomość:</label>
         <textarea id="message" v-model="formData.message" required></textarea>
         
-        <button type="submit">Wyślij</button>
+        <button type="submit" class="Button-SP">Wyślij</button>
       </form>
     </section>
+    <!-- End of Contact -->
 
     <!-- Footer -->
     <footer>
-      <div class="social-links">
+      <div class="Social-links">
         <a href="https://www.facebook.com/">
           <img src="C:\Users\tazor\Documents\GitHub\SoundDesignWeb01\Web01\src\assets\fb.png" class="icons" alt="Facebook" />
         </a>
@@ -157,6 +169,8 @@
       </div>
       <p>&copy; 2024 Reserved to Wojciech Faber</p>
     </footer>
+<!-- End of Footer -->
+  
   </div>
 </template>
 
